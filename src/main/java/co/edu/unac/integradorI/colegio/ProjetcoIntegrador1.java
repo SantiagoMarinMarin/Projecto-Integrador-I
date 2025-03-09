2ce4b4d226ed53efe14817bd7dd17ce47f9033d1
Polasana2020 <carlosf.polanias@unac.edu.co>
2025-03-09 17:15:30 -0500

    Añado el login y coloco paquete para almacenar las imagenes que se utilizen.

--- src/main/java/co/edu/unac/integradorI/colegio/ProjetcoIntegrador1.java
+++ src/main/java/co/edu/unac/integradorI/colegio/ProjetcoIntegrador1.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.projetcointegrador1;
+package co.edu.unac.integradorI.colegio;
 
 /**
  *
